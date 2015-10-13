58c0f446d0aa7d3426c85b459890f85ae6be2158
gulci <sebastian.gulczynski.poznan@gmail.com>
2015-10-13 14:18:42 +0200

    2015-10-13 initial commit

---
+++ pythink01.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+print(type(31))
+print(type(2.01))
+print(type("hello"))
+
+# tworzymy tuple, zera będą obcięte do 0 - działa tylko dla zer
+nums = 1, 000, 000
+
+print(nums)
+
+# słowa kluczowe: False, None, True, and, as, assert, break, class, continue, def, del, elif, else, except, finally, for, from, global, if, import, in, is, lambda, nonlocal, not, or, pass, raise, return, try, while, with, yield
+
+# statement - kawałek kodu, który daje jakiś efekt; nie mają wartości
+
+# PEMDAS - nawiasy, potęgi, mnożenie, dzielenie, dodawanie, odejmowanie
+# ten sam priorytet - od lewej do prawej, z wyjątkiem potęgowania
+
+# Syntax Error
+# Runtime Error (exceptions)
+# Semantic error
+
+x = y = 1
+
+# 3.2 Math functions
